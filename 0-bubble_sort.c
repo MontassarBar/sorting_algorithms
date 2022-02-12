ce6779adec08d8af4039e56b7e68c64195c8ca39
MontassarBar <montassarbarbouchi7@gmail.com>
2022-02-12 19:17:42 +0100

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ *swap - swap two integers
+ *@x: int
+ *@y: int
+ */
+void swap(int *x, int *y)
+{
+int temp = *x;
+*x = *y;
+*y = temp;
+}
+/**
+ *bubble_sort - sorts an array of integers in ascending order using
+the Bubble sort algorithm
+ *@array: the array to be sorted
+ *@size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t x, y;
+for (x = 0; x < size - 1; x++)
+{
+for (y = 0; y < size - x - 1; y++)
+{
+if (array[y] > array[y + 1])
+swap(&array[y], &array[y + 1]);
+print_array(array, size);
+}
+}
+}
