7e27d621fc722d6003ea112fde5e889ecec07986
MontassarBar <montassarbarbouchi7@gmail.com>
2022-02-15 20:42:34 +0100

    insertion_sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ *swap_nodes - swap 2 nodes
+ *@node1: first node
+ *@node2: second node
+ */
+void swap_nodes(listint_t *node1, listint_t *node2)
+{
+if (node2->next)
+node2->next->prev = node1;
+if (node1->prev)
+node1->prev->next = node2;
+node2->prev = node1->prev;
+node1->next = node2->next;
+node2->next = node1;
+node1->prev = node2;
+}
+/**
+ *insertion_sort_list - sorts an array of integers in ascending order
+ *@list: list
+ **/
+void insertion_sort_list(listint_t **list)
+{
+listint_t *h, *n;
+h = *list;
+while (h)
+{
+n = h;
+while (n->prev)
+{
+if (n->prev->n > n->n)
+{
+swap_nodes(n->prev, n);
+if (n->prev == NULL)
+*list = n;
+print_list(*list);
+}
+else
+n = n->prev;
+}
+h = h->next;
+}
+}
